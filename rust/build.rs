e937f46809ffc7a8e1588dd32acaf9e100eef178
Naoki Fujita <fujitanaoki0718@gmail.com>
2023-04-06 01:05:46 +0900

    refact: protosフォルダに集約する

--- rust/build.rs
+++ rust/build.rs
@@ -1,4 +1,4 @@
 fn main() -> std::io::Result<()> {
-    prost_build::compile_protos(&["src/board.proto"], &["src/"])?;
+    prost_build::compile_protos(&["protos/board.proto"], &["protos/"])?;
     Ok(())
 }
