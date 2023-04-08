6e6da471bd9a4b0ccca714dd2667aa71795ea3de
Naoki Fujita <fujitanaoki0718@gmail.com>
2023-04-08 16:46:53 +0900

    feat: protobufファイルは言語間で共用可能だと表現する

--- rust/build.rs
+++ rust/build.rs
@@ -1,11 +1,11 @@
 fn main() -> std::io::Result<()> {
     prost_build::compile_protos(
         &[
-            "protos/protobuf/v1/migrate.proto",
-            "protos/protobuf/v1/exhaustive.proto",
-            "protos/protobuf/v2/migrate.proto",
+            "../protos/protobuf/v1/migrate.proto",
+            "../protos/protobuf/v1/exhaustive.proto",
+            "../protos/protobuf/v2/migrate.proto",
         ],
-        &["protos/"],
+        &["../protos/"],
     )?;
     Ok(())
 }
