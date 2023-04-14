10eb4b774f98847cf5da56c0df398c70e8096bd5
Naoki Fujita <fujitanaoki0718@gmail.com>
2023-04-14 23:04:45 +0900

    feat: send druid protobuf tutorial protobuf message

--- confluent/rust/build.rs
+++ confluent/rust/build.rs
@@ -1,6 +1,6 @@
 fn main() -> std::io::Result<()> {
     prost_build::compile_protos(
-        &["../../protos/protobuf/v1/migrate.proto"],
+        &["../../protos/protobuf/v1/metrics.proto"],
         &["../../protos/"],
     )?;
     Ok(())
--- confluent/rust/src/send_protobuf.rs
+++ confluent/rust/src/send_protobuf.rs
@@ -3,43 +3,40 @@ use prost::Message as _;
 use rand::Rng as _;
 use rdkafka::{consumer::Consumer as _, producer::FutureRecord, Message as _};
 use rust_studies_kafka::{
-    create_future_producer, create_stream_consumer, create_tokio_rt, init_logger,
-    protobuf::{Migrate, MigrateChild},
+    create_future_producer, create_stream_consumer, create_tokio_rt, init_logger, protobuf::Metrics,
 };
 use std::time::Duration;
 
 async fn producer(brokers: &str) {
     let producer = create_future_producer(brokers);
-    let mut cnt = 0i64;
     loop {
-        let record = Migrate {
-            i: cnt,
-            t: Some(prost_types::Timestamp::from(std::time::SystemTime::now())),
-            bc1: vec![MigrateChild {
-                d: rand::thread_rng().gen_range(100.0..150.0),
-                f: rand::thread_rng().gen_range(0.1..5.0),
-            }],
-            bc2: vec![],
-            d: rand::thread_rng().gen_range(0.1..3.0),
+        let record = Metrics {
+            time: chrono::Utc::now().to_rfc3339(),
+            unit: "ms".to_string(),
+            http_method: "GET".to_string(),
+            value: rand::thread_rng().gen_range(1..5),
+            http_code: "200".to_string(),
+            page: "index".to_string(),
+            metric_type: "counter".to_string(),
+            server: "localhost".to_string(),
         }
         .encode_to_vec();
-        let record = FutureRecord::to("protobuf")
+        let record = FutureRecord::to("metrics_pb")
             .key("some key")
             .payload(&record);
         let res = producer.send(record, Duration::from_secs(0)).await.unwrap();
         dbg!(res);
         tokio::time::sleep(Duration::from_secs(1)).await;
-        cnt += 1;
     }
 }
 
 async fn consumer(brokers: &str) {
     let consumer = create_stream_consumer(brokers);
-    consumer.subscribe(&["protobuf"]).unwrap();
+    consumer.subscribe(&["metrics_pb"]).unwrap();
     consumer
         .stream()
         .try_for_each(|borrow_message| async move {
-            let record = Migrate::decode(borrow_message.payload().unwrap()).unwrap();
+            let record = Metrics::decode(borrow_message.payload().unwrap()).unwrap();
             dbg!(record);
             Ok(())
         })
@@ -48,9 +45,9 @@ async fn consumer(brokers: &str) {
 }
 
 async fn run() {
-    let brokers = "localhost:39092";
-    tokio::spawn(producer(brokers));
-    consumer(brokers).await;
+    let brokers = "localhost:9092";
+    producer(brokers).await;
+    // consumer(brokers).await;
 }
 
 fn main() {
