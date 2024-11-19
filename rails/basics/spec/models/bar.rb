e4763457b3039c049542c969a86ba5ef1cc2511f
Naoki Fujita <fujitanaoki0718@gmail.com>
2024-11-20 07:23:19 +0900

    feat: github actions CI PoC

---
+++ rails/basics/spec/models/bar.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe 'bar' do
+    it { expect(4 + 5).to eq 9 }
+end
---
+++ rails/basics/spec/models/foo.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'foo' do
+    it { expect(2 + 2).to eq 4 }
+end
