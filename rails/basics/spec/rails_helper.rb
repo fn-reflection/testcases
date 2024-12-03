5d98a32f63a5e8e407a06efc86cc483fb4fe653c
Naoki Fujita <fujitanaoki0718@gmail.com>
2024-12-03 16:45:06 +0900

    feat: rspecのデフォルトに合わせる

---
+++ rails/basics/spec/rails_helper.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+ENV['RAILS_ENV'] = 'test'
+require_relative '../config/environment'
+require 'rspec/rails'
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+RSpec.configure do |config|
+  config.fixture_paths = [
+    Rails.root.join('spec/fixtures')
+  ]
+  config.use_transactional_fixtures = true
+  config.filter_rails_from_backtrace!
+end
